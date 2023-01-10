7d10dd4c0f1c1bf0b066e58022a652c412bcdba9
Rafael Leites Luchese <rlluchese@gmail.com>
2023-01-10 10:39:46 -0300

    App component

---
+++ jsx/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function App(){
+    // return <input style={{ border: '3px solid red'}} type="number" min={1} max={10} />;
+    return <h1>Hello there!</h1>;
+}
+
+export default App;
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-function App(){
-    return <h1>Hi there!</h1>;
-}
-
 root.render(<App />);
